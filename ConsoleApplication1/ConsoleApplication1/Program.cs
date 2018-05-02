c94e027ae35e039760267de335f57dd08af54fa5
MountainVole <r.austin.hager@gmail.com>
2018-05-02 10:33:04 -0600

    Initial creation

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("I will say what you say!");
+            // Keep the console window open in debug mode.
+            Console.Write("  ");
+            string testString;
+            testString=Console.ReadLine();
+            Console.WriteLine("    " + testString);
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
